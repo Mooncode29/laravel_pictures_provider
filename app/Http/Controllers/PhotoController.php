bbe40e969b7d2da024dd9cd42172372933de45cf
Mooncode29 <irene.ravelonarivo@gmail.com>
2017-02-22 16:21:56 +0100

    cration controller, affichage des photos en mode aléatoire

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -4,7 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class PhotoController extends Controller
 {
-    //
+	public function displayPhoto (){
+	    $dossier = 'photos_simplon';
+	    $tab_dossier = glob($dossier.'/*.jpg');
+	   	$path = $tab_dossier[array_rand($tab_dossier)];
+	   	return view('home',['photo'=>$path]);
+	   
+	}
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', 'PhotoController@displayPhoto');
+// Route::get('/public/photos_simplon',function(){
+// 	$dossier = '../public/photos_simplon';
+// 	    $tab_dossier = glob($dossier.'/*.jpg');
+// 	   // var_dump($tab_dossier)
+// 	   $path = $tab_dossier[array_rand($tab_dossier)];
+// require __DIR__.'../public/photos_simplon/DHP_6980.jpg';
+// });
