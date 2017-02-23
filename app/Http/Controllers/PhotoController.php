7f2d406788991026c701cf714d6ccb65c2a2e977
Mooncode29 <irene.ravelonarivo@gmail.com>
2017-02-23 14:44:20 +0100

    suppression photo test, verification route, suppression home;php

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -7,11 +7,26 @@ use Illuminate\Http\Request;
 
 class PhotoController extends Controller
 {
+	private $dossier;
+	private $tab_dossier;
+	private $path;
+	public function getDossier (){
+		return $this->dossier;
+	}
+	public function getTab (){
+		return $this->tab_dossier;
+	}
+	public function getPath(){
+		return $this->path;
+	}
 	public function displayPhoto (){
-	    $dossier = 'photos_simplon';
-	    $tab_dossier = glob($dossier.'/*.jpg');
-	   	$path = $tab_dossier[array_rand($tab_dossier)];
-	   	return view('home',['photo'=>$path]);
-	   
+	    $this->dossier = 'photos_simplon';
+	    $this->$tab_dossier = glob($dossier.'/*.jpg');
+	   	$this->$path = $tab_dossier[array_rand($tab_dossier)];
+	   	return view('home',['photo'=>$path]);	   
+	}
+	public function cropPhoto (){
+		
+
 	}
 }
